116de6495338a5349fafe1c682990c39d0c57488
Panni <simaianna3@gmail.com>
2019-05-07 15:53:11 +0200

    started search input

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 
 import "./App.css";
 import Main from "./components/Main";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
